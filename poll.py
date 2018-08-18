35658af205ced9bbf389e4c6464ed70c9a6e9ad0
naveenr414 <dsfan414@gmail.com>
2018-08-17 23:07:00 -0400

    Able to find type of election and election state

--- poll.py
+++ poll.py
@@ -1,9 +1,23 @@
+import geography
+
 class Poll:
     def __init__(self):
         self.title = ""
         self.company = ""
-        self.results = ""
         self.spread = ""
+        self.details = ""
+        self.results = ""
+
+    def parseData(self):
+        #Figure out if its a state ballot
+        self.state = ""
+        self.electionType = ""
+    
+        for state in geography.stateList:
+            if state in self.title:
+                self.state = state
+                self.electionType = self.title.replace(self.state+" ","")
+                break
 
     def __str__(self):
         return self.title+" by "+self.company + " predicted "+self.spread
--- scraper.py
+++ scraper.py
@@ -2,26 +2,37 @@ from bs4 import BeautifulSoup
 import urllib.request as ur
 from poll import Poll
 
-sock = ur.urlopen("https://www.realclearpolitics.com/epolls/latest_polls/")
-soup = BeautifulSoup(sock,"html5lib")
+def scrapePolls():
+    sock = ur.urlopen("https://www.realclearpolitics.com/epolls/latest_polls/")
+    soup = BeautifulSoup(sock,"html5lib")
 
-polls = soup.findAll("tr",{"class":"alt"})
-pollList = []
+    polls = soup.findAll("tr",{"class":"alt"})
+    pollList = []
 
-for i in polls:
-    p = Poll()
-    
-    pollTitle = i.find("td",{"class":"lp-race"}).find("a").contents[0]
-    pollCompany = i.find("td",{"class":"lp-poll"}).find("a").contents[0]
-    pollResults = i.find("td",{"class":"lp-results"}).find("a").contents[0]
-    pollSpread = i.find("td",{"class":"lp-spread"}).find("span").contents[0]
+    for i in polls:
+        p = Poll()
+        
+        pollTitle = i.find("td",{"class":"lp-race"}).find("a").contents[0]
+        pollCompany = i.find("td",{"class":"lp-poll"}).find("a").contents[0]
+        pollResults = i.find("td",{"class":"lp-results"}).find("a").contents[0]
+        pollSpread = i.find("td",{"class":"lp-spread"}).find("span").contents[0]
 
-    p.title = pollTitle
-    p.company = pollCompany
-    p.results = pollResults
-    p.spread = pollSpread
+        if("-" in pollTitle):
+            pollDetails = pollTitle.split("-")[1]
+            pollTitle = pollTitle.split("-")[0].strip()
+            p.details = pollDetails.strip()
 
-    pollList.append(p)
+        p.title = pollTitle
+        p.company = pollCompany
+        p.results = pollResults
+        p.spread = pollSpread
 
+        p.parseData()
+
+        pollList.append(p)
+
+    return pollList
+
+pollList = scrapePolls()
 for i in pollList:
-    print(i)
+    print(i.electionType,i.title)
