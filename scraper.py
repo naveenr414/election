fb71c64d81cec4d117d86bfc8b2f2c5d03b4d4dd
naveenr414 <dsfan414@gmail.com>
2018-08-21 23:32:02 -0400

    Scraping polls details

--- scraper.py
+++ scraper.py
@@ -1,9 +1,11 @@
 from bs4 import BeautifulSoup
 import urllib.request as ur
 from poll import Poll
+from urllib.parse import urljoin
 
 def scrapePolls():
-    sock = ur.urlopen("https://www.realclearpolitics.com/epolls/latest_polls/elections/")
+    base = "https://www.realclearpolitics.com/epolls/latest_polls/elections/"
+    sock = ur.urlopen(base)
     soup = BeautifulSoup(sock,"html5lib")
 
     tables = soup.findAll("table",{"class":"sortable"})
@@ -23,6 +25,20 @@ def scrapePolls():
         pollCompany = i.find("td",{"class":"lp-poll"}).find("a").contents[0]
         pollResults = i.find("td",{"class":"lp-results"}).find("a").contents[0]
         pollSpread = i.find("td",{"class":"lp-spread"}).find("span").contents[0]
+
+        href = urljoin(base,i.find("td",{"class":"lp-results"}).find("a")['href'])
+        detailSoup = BeautifulSoup(ur.urlopen(href),"html5lib")
+        pollDetails = detailSoup.findAll("tr")
+
+        l = []
+        for j in pollDetails:
+            if(j.has_attr('data-id')):
+                l.append(j)
+        pollDetails = l
+
+        for j in pollDetails:
+            k = j.find("td",{"class":"noCenter"}).find("a",{"class":"normal_pollster_name"}).contents
+            print(pollTitle,k)
 
         if("-" in pollTitle):
             pollDetails = pollTitle.split("-")[1]
